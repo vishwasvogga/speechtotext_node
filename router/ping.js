a57ea92f4b794c45512891ae73750baaa7d194eb
Vishwas poojary k <vishwasvogga@gmail.com>
2022-05-21 15:16:08 +0530

    ping route added

---
+++ router/ping.js
@@ -0,0 +1,39 @@
+
+module.exports = [
+    {
+        method: 'GET',
+        path: '/ping',
+        config: {
+            handler: function (req, res) {
+                return res.response({ success: true }).code(200);
+            },
+            auth: {
+                strategy: 'jwt',
+                mode: 'optional'
+            }
+        }
+    },
+    {
+        method: 'GET',
+        path: '/hault',
+        config: {
+            handler: async function (req, res) {
+                let temp = await hault();
+                return res.response(temp).code(200);
+            },
+            auth: {
+                strategy: 'jwt',
+                mode: 'optional'
+            }
+        }
+    }
+
+]
+
+async function hault(){
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
+            return resolve({ success: true , delay : 120000 })
+        },120000)
+    })
+}
